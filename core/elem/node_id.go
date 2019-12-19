e4ffed601a73744c5ace8c6afd55193ad6517ed8
workerwork <workerwork@qq.com>
2019-12-19 17:38:13 +0800

    add fseid

--- core/elem/node_id.go
+++ core/elem/node_id.go
@@ -1,11 +1,17 @@
 package elem
 
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+)
+
 type NodeIDType byte
 
 const (
-	IPv4Addr NodeIDType = 0
-	IPv6Addr NodeIDType = 1
-	FQDN     NodeIDType = 2
+	NodeIDTypeIPv4Addr NodeIDType = 0
+	NodeIDTypeIPv6Addr NodeIDType = 1
+	NodeIDTypeFQDN     NodeIDType = 2
 )
 
 type NodeID struct {
@@ -19,6 +25,7 @@ func NewIPv4NodeID(ipv4 []byte) *NodeID {
 	return &NodeID{
 		EType:   IETypeNodeID,
 		ELength: uint16(4),
+		NodeIDType: NodeIDTypeIPv4Addr,
 		NodeID:  ipv4,
 	}
 }
@@ -33,7 +40,20 @@ func DecodeNodeID(data []byte, len uint16) *NodeID {
 }
 
 func EncodeNodeID(nodeID NodeID) []byte {
-	return setBuffer(nodeID.EType, nodeID.ELength, setValue(byte(nodeID.NodeIDType), nodeID.NodeID))
+	buf := bytes.NewBuffer([]byte{})
+	if err := binary.Write(buf, binary.BigEndian, nodeID.EType);err != nil {
+		log.Println("err")	//TODO::
+	}
+	if err := binary.Write(buf, binary.BigEndian, nodeID.ELength);err != nil {
+		log.Println("err")	//TODO::
+	}
+	if err := binary.Write(buf, binary.BigEndian, nodeID.NodeIDType);err != nil {
+		log.Println("err")	//TODO::
+	}
+	if err := binary.Write(buf, binary.BigEndian, nodeID.NodeID);err != nil {
+		log.Println("err")	//TODO::
+	}
+	return buf.Bytes()
 }
 
 //判断是否含有NodeID
--- core/elem/util.go
+++ core/elem/util.go
@@ -3,26 +3,14 @@ package elem
 import (
 	"bytes"
 	"encoding/binary"
+	"log"
 )
 
 func getValue(data []byte, len uint16) []byte {
 	buf := bytes.NewBuffer(data)
 	value := make([]byte, len)
-	binary.Read(buf, binary.BigEndian, &value)
+	if err := binary.Read(buf, binary.BigEndian, &value);err != nil {
+		log.Println("err")	//TODO::
+	}
 	return value
-}
-
-func setBuffer(t IEType, l uint16, v []byte) []byte {
-	buf := bytes.NewBuffer([]byte{})
-	binary.Write(buf, binary.BigEndian, t)
-	binary.Write(buf, binary.BigEndian, l)
-	binary.Write(buf, binary.BigEndian, v)
-	return buf.Bytes()
-}
-
-func setValue(t byte, v []byte) []byte {
-	buf := bytes.NewBuffer([]byte{})
-	binary.Write(buf, binary.BigEndian, t)
-	binary.Write(buf, binary.BigEndian, v)
-	return buf.Bytes()
 }
