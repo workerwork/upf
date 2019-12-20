6009ce5fc1e7ef168d9297cdd6ae3841c91dee97
workerwork <workerwork@qq.com>
2019-12-20 15:15:51 +0800

    add urr

--- core/elem/urr_id.go
+++ core/elem/urr_id.go
@@ -1,1 +1,30 @@
 package elem
+
+type URRID struct {
+	EType   IEType
+	ELength uint16
+	URRID  []byte	//4byte
+}
+
+
+func DecodeURRID(data []byte, len uint16) *URRID {
+	return &URRID{
+		EType:   IETypeURRID,
+		ELength: len,
+		URRID:  getValue(data, len),
+	}
+}
+
+func EncodeURRID(urrID URRID) []byte {
+	return setValue(urrID.EType, urrID.ELength, urrID.URRID)
+}
+
+//判断是否含有URRID
+func HasURRID(urrID URRID) bool {
+	if urrID.EType == 0 {
+		return false
+	}
+	return true
+}
+
+
