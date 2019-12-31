e710b553e019be0e61d06cf4f36ef0140df57f2f
workerwork <workerwork@qq.com>
2019-12-31 10:27:16 +0800

    add far_id.go

--- core/elem/far_id.go
+++ core/elem/far_id.go
@@ -1,1 +1,27 @@
 package elem
+
+type FARID struct {
+	EType   IEType
+	ELength uint16
+	FARID  []byte //4byte
+}
+
+func DecodeFARID(data []byte, len uint16) *FARID {
+	return &FARID{
+		EType:   IETypeFARID,
+		ELength: len,
+		FARID:  getValue(data, 4),
+	}
+}
+
+func EncodeFARID(f FARID) []byte {
+	return setValue(f.EType, f.ELength, f.FARID)
+}
+
+//判断是否含有FARID
+func HasFARID(f FARID) bool {
+	if f.EType == 0 {
+		return false
+	}
+	return true
+}
