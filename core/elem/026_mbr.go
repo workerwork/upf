2ded36fa6c8a5df118d7e0b5af97b7ac73d1f27b
workerwork <workerwork@qq.com>
2020-01-02 11:10:07 +0800

    add 026_mbr.go

---
+++ core/elem/026_mbr.go
@@ -0,0 +1,29 @@
+package elem
+
+type MBR struct {
+	EType   IEType
+	ELength uint16
+	ULMBR   []byte //5bytes
+	DLMBR   []byte //5bytes
+}
+
+func DecodeMBR(data []byte, len uint16) *MBR {
+	return &MBR{
+		EType:   IETypeMBR,
+		ELength: len,
+		ULMBR:   getValue(data, 5),
+		DLMBR:   getValue(data, 5),
+	}
+}
+
+func EncodeMBR(m MBR) []byte {
+	return setValue(g.EType, g.ELength, m.ULMBR, m.DLMBR)
+}
+
+//判断是否含有MBR
+func HasGateMBR(m MBR) bool {
+	if m.EType == 0 {
+		return false
+	}
+	return true
+}
