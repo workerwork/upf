618605f46c70cfc93f19c9ad282776e7ebf941b1
workerwork <workerwork@qq.com>
2020-01-02 11:43:59 +0800

    add 113_pdn_type.go

--- core/elem/113_pdn_type.go
+++ core/elem/113_pdn_type.go
@@ -1,1 +1,38 @@
 package elem
+
+type PDNTypeType byte
+
+const (
+	_                   PDNTypeType = iota //0
+	PDNTypeTypeIPv4                        //1
+	PDNTypeTypeIPv6                        //2
+	PDNTypeTypeIPv4v6                      //3
+	PDNTypeTypeNonIP                       //4
+	PDNTypeTypeEthernet                    //5
+)
+
+type PDNType struct {
+	EType   IEType
+	ELength uint16
+	PDNType PDNTypeType
+}
+
+func DecodePDNType(data []byte, len uint16) *PDNType {
+	return &PDNType{
+		EType:   IETypePDNType,
+		ELength: len,
+		PDNType: PDNTypeType(getValue(data, 1)[0]),
+	}
+}
+
+func EncodePDNType(p PDNType) []byte {
+	return setValue(p.EType, p.ELength, p.PDNType)
+}
+
+//判断是否含有PDNType
+func HasPDNType(p PDNType) bool {
+	if p.EType == 0 {
+		return false
+	}
+	return true
+}
